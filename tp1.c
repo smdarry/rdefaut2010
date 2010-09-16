9690cd05ed94a8a2397223bb4765fdc71ce7cf68
Steve Darry <stevedarry@yahoo.ca>
2010-09-16 08:18:51 -0400

    Refactored histogram

--- tp1.c
+++ tp1.c
@@ -1,63 +1,39 @@
 #include "cv.h"
 #include "highgui.h"
+#include "histogram.h"
 #include <stdio.h>
 
 #define GET_PIXEL_PTR_AT(img, row, col) img->imageData+row*3+col
-#define GRAY_LEVELS 256
-
-typedef struct _hist
-{
-    int _red[GRAY_LEVELS];
-    int _green[GRAY_LEVELS];
-    int _blue[GRAY_LEVELS];
-} Histogram;
-
-void initHistogram(Histogram* h)
-{
-    int i;
-    for(i = 0; i < GRAY_LEVELS; i++)
-    {
-        h->_red[i] = 0;
-        h->_green[i] = 0;
-        h->_blue[i] = 0;
-    }
-}
-
-void writeHistogram(char* filename, Histogram* h)
-{
-    FILE* fp = fopen(filename, "w+");
-    if(fp == NULL)
-    {
-        char error[256];
-        sprintf(error, "Could not create file '%s'\n", filename);
-        fprintf(stderr, error);
-
-        return;
-    }
-
-    int i;
-    for(i = 0; i < GRAY_LEVELS-1; i++)
-    {
-        fprintf(fp, "%d,", (int)h->_red[i]);
-    }
-    fprintf(fp, "%d\n", (int)h->_red[GRAY_LEVELS-1]);
-
-    fclose(fp);
-}
+#define IMAGE_COUNT 795
 
 int main( int argc, char** argv )
 {
     // 3-channel histograms for each target pixel 
-    Histogram h10_10, h596_265, h217_137;
+    Histogram h10_10_r, h10_10_g, h10_10_b;
+    Histogram h596_265_r, h596_265_g, h596_265_b;
+    Histogram h217_137_r, h217_137_g, h217_137_b;
 
-    initHistogram(&h10_10);
-    initHistogram(&h596_265);
-    initHistogram(&h217_137);
+    initHistogram(&h10_10_r);
+    initHistogram(&h10_10_g);
+    initHistogram(&h10_10_b);
+
+    initHistogram(&h596_265_r);
+    initHistogram(&h596_265_g);
+    initHistogram(&h596_265_b);
+
+    initHistogram(&h217_137_r);
+    initHistogram(&h217_137_g);
+    initHistogram(&h217_137_b);
+
+    // Chronogram for each pixel
+    char c10_10_r[IMAGE_COUNT], c10_10_g[IMAGE_COUNT], c10_10_b[IMAGE_COUNT];
+    char c596_265_r[IMAGE_COUNT], c596_265_g[IMAGE_COUNT], c596_265_b[IMAGE_COUNT];
+    char c217_137_r[IMAGE_COUNT], c217_137_g[IMAGE_COUNT], c217_137_b[IMAGE_COUNT];
 
     int i, row;
-    char filename[255];
+    char filename[256];
     IplImage* img;
-    for(i = 0; i < 795; i++)
+    for(i = 0; i < IMAGE_COUNT; i++)
     {
         sprintf(filename, "./View_008/frame_%04d.jpg", i);
 
@@ -75,33 +51,48 @@ int main( int argc, char** argv )
         ptr = GET_PIXEL_PTR_AT(img, 10, 10);
 
         // Update each histogram for that pixel
-        h10_10._blue[*ptr]++;
-        h10_10._green[*(ptr+1)]++;
-        h10_10._red[*(ptr+2)]++;
+        h10_10_b.dist[*ptr]++;
+        h10_10_g.dist[*(ptr+1)]++;
+        h10_10_r.dist[*(ptr+2)]++;
+
+        // Compile chronogram values
+        c10_10_b[i] = *ptr;
+        c10_10_g[i] = *(ptr+1);
+        c10_10_r[i] = *(ptr+2);
 
         // Extract pixel at location [596, 265]
         ptr = GET_PIXEL_PTR_AT(img, 596, 265);
 
         // Update each histogram for that pixel
-        h596_265._blue[*ptr]++;
-        h596_265._green[*(ptr+1)]++;
-        h596_265._red[*(ptr+2)]++;
+        h596_265_b.dist[*ptr]++;
+        h596_265_g.dist[*(ptr+1)]++;
+        h596_265_r.dist[*(ptr+2)]++;
+
+        // Compile chronogram values
+        c596_265_b[i] = *ptr;
+        c596_265_g[i] = *(ptr+1);
+        c596_265_r[i] = *(ptr+2);
 
         // Extract pixel at location [217, 137]
         ptr = GET_PIXEL_PTR_AT(img, 217, 137);
 
         // Update each histogram for that pixel
-        h217_137._blue[*ptr]++;
-        h217_137._green[*(ptr+1)]++;
-        h217_137._red[*(ptr+1)]++;
+        h217_137_b.dist[*ptr]++;
+        h217_137_g.dist[*(ptr+1)]++;
+        h217_137_r.dist[*(ptr+2)]++;
+
+        // Compile chronogram values
+        c217_137_b[i] = *ptr;
+        c217_137_g[i] = *(ptr+1);
+        c217_137_r[i] = *(ptr+2);
 
         cvReleaseImage(&img);
     }
 
     // Output histograms in CSV format
-    writeHistogram("output/histo10x10.csv", &h10_10);
-    writeHistogram("output/histo596x265.csv", &h596_265);
-    writeHistogram("output/histo217x137.csv", &h217_137);
+    writeHistogram(&h10_10_r, "output/histo10x10.csv");
+    writeHistogram(&h596_265_r, "output/histo596x265.csv");
+    writeHistogram(&h217_137_r, "output/histo217x137.csv");
 
     return 0;
 }
