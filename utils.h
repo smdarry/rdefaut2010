2901d208610f361a103e0cdac3c076e9f5ca47f2
Steve Darry <stevedarry@yahoo.ca>
2010-09-23 09:15:10 -0400

    Added printing functions

--- utils.h
+++ utils.h
@@ -8,6 +8,12 @@ void printFrame(IplImage* frame, int channel, char* filename)
 {
     FILE* fp = fopen(filename, "w+");
 
+    // Print index row
+    int i;
+    for(i = 0; i < frame->width-1; i++)
+        fprintf(fp, "%d,", i);
+    fprintf(fp, "%d\n", frame->width-1);
+
     int row, col, step = frame->widthStep;
     for(row = 0; row < frame->height; row++)
     {
@@ -32,6 +38,44 @@ void printByteFrame(IplImage* frame, int channel, char* filename)
         {
             uchar c = ((uchar*)(frame->imageData + step*row))[col*3];
             fprintf(fp, "%d,", c);
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
+
+void printPatch(IplImage* frame, int channel, char* filename, int beginX, int beginY, int endX, int endY)
+{
+    uchar pixels[endX - beginX];
+
+    FILE* fp = fopen(filename, "w+");
+
+    int i = 0, row, col, step = frame->widthStep;
+    for(row = beginY; row <= endY; row++)
+    {
+        for(col = beginX; col <= endX; col++)
+        {
+            pixels[i] = ((uchar*)(frame->imageData + step*row))[col*3];
+            fprintf(fp, "%d,", pixels[i]);
+            i++;
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
+
+void printPatchF(IplImage* frame, int channel, char* filename, int beginX, int beginY, int endX, int endY)
+{
+    FILE* fp = fopen(filename, "w+");
+
+    int i = 0, row, col, step = frame->widthStep;
+    for(row = beginY; row <= endY; row++)
+    {
+        for(col = beginX; col <= endX; col++)
+        {
+            float f = ((float*)(frame->imageData + step*row))[col*3];
+            fprintf(fp, "%.2f,", f);
+            i++;
         }
         fprintf(fp, "\n");
     }
