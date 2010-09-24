82865c3e664b47ecc2299e1da931c49ed534d797
Steve Darry <stevedarry@yahoo.ca>
2010-09-24 15:59:06 -0400

    Made R scrit accept parameters and added legend

--- scripts/chrono.R
+++ scripts/chrono.R
@@ -1,12 +1,10 @@
-count <- 795
+cvs_file = paste("output/chrono_", row, "x", column, ".csv", sep="");
+title = paste("Chronogramme du pixel [", row, ", ", column, "]", sep="");
 
-## A <- matrix(scan("../output/chrono_10x10.csv", n = 3 * count, sep = ','), count, 3)
-## A <- matrix(scan("../output/chrono_596x265.csv", n = 3 * count, sep = ','), count, 3)
-A <- matrix(scan("../output/chrono_217x137.csv", n = 3 * count, sep = ','), count, 3)
+A <- matrix(scan(cvs_file, n = 3 * count, sep = ','), count, 3);
+A <- t(A);
 
-A <- t(A)
-
-plot(A[1,], type="l", col="blue", ylim=c(0, 255), xlab="Image no.", ylab="Intensite", main="Chronogramme du pixel [10,10]")
+plot(A[1,], type="l", col="blue", ylim=c(0, 255), xlab="Image no.", ylab="Intensite", main=title)
 lines(A[2,], type="l", col="green")
 lines(A[3,], type="l", col="red")
 
@@ -16,6 +14,12 @@ mean_blue = array(mean(A[1,]), c(1, count))
 mean_green = array(mean(A[2,]), c(1, count))
 mean_red = array(mean(A[3,]), c(1, count))
 
-## lines(x, mean_blue, col="blue")
-## lines(x, mean_green, col="green")
-## lines(x, mean_red, col="red")
+lines(x, mean_blue, col="blue", lty=3)
+lines(x, mean_green, col="green", lty=3)
+lines(x, mean_red, col="red", lty=3)
+
+label_blue = paste("Mean = ", format(mean(A[1,]), digits=4), sep="")
+label_green = paste("Mean = ", format(mean(A[2,]), digits=4), sep="")
+label_red = paste("Mean = ", format(mean(A[3,]), digits=4), sep="")
+
+legend(x=1, y=255, legend=c(label_blue, label_green, label_red), lty=c(3, 3, 3), col=c("blue", "green", "red"))
