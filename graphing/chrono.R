6399865c16a2e48274681ede98c7b434b5d23971
Steve Darry <stevedarry@yahoo.ca>
2010-09-25 14:07:47 -0400

    Added high and low sigma lines

--- graphing/chrono.R
+++ graphing/chrono.R
@@ -12,21 +12,44 @@ x = 1:count
 
 means = c(mean(A[1,]), mean(A[2,]), mean(A[3,]))
 sds = c(sd(A[1,]), sd(A[2,]), sd(A[3,]))
+
+
+## Mean lines
 mean_blue = array(means[1], c(1, count))
 mean_green = array(means[2], c(1, count))
 mean_red = array(means[3], c(1, count))
 
-lines(x, mean_blue, col="blue", lty=3)
-lines(x, mean_green, col="green", lty=3)
-lines(x, mean_red, col="red", lty=3)
+lines(x, mean_blue, col="blue", lty=1)
+lines(x, mean_green, col="green", lty=1)
+lines(x, mean_red, col="red", lty=1)
 
+
+## Standard deviation lines
+high_blue = array(means[1] + sds[1], c(1, count))
+high_green = array(means[2] + sds[2], c(1, count))
+high_red = array(means[3] + sds[3], c(1, count))
+
+lines(x, high_blue, col="blue", lty=3)
+lines(x, high_green, col="green", lty=3)
+lines(x, high_red, col="red", lty=3)
+
+low_blue = array(means[1] - sds[1], c(1, count))
+low_green = array(means[2] - sds[2], c(1, count))
+low_red = array(means[3] - sds[3], c(1, count))
+
+lines(x, low_blue, col="blue", lty=3)
+lines(x, low_green, col="green", lty=3)
+lines(x, low_red, col="red", lty=3)
+
+
+## Legends
 label_blue = paste("Mu = ", format(means[1], digits=4), sep="")
 label_green = paste("Mu = ", format(means[2], digits=4), sep="")
 label_red = paste("Mu = ", format(means[3], digits=4), sep="")
 
-sd_label_blue = paste("Sigma = ", format(sds[1], digits=4), sep="")
-sd_label_green = paste("Sigma = ", format(sds[2], digits=4), sep="")
-sd_label_red = paste("Sigma = ", format(sds[3], digits=4), sep="")
+sd_label_blue = paste("3*(Sigma = ", format(sds[1], digits=4), ")", sep="")
+sd_label_green = paste("3*(Sigma = ", format(sds[2], digits=4), ")", sep="")
+sd_label_red = paste("3*(Sigma = ", format(sds[3], digits=4), ")", sep="")
 
-legend(x=1, y=255, legend=c(label_blue, label_green, label_red), lty=c(3, 3, 3), col=c("blue", "green", "red"))
+legend(x=1, y=255, legend=c(label_blue, label_green, label_red), lty=c(1, 1, 1), col=c("blue", "green", "red"))
 legend(x=1, y=200, legend=c(sd_label_blue, sd_label_green, sd_label_red), lty=c(3, 3, 3), col=c("blue", "green", "red"))
