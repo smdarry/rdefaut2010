2662929a567d98c83fa78f2ba071e13170510df4
Steve Darry <stevedarry@yahoo.ca>
2010-10-02 08:56:00 -0400

    First commit

---
+++ bbox.c
@@ -0,0 +1,34 @@
+#include "cv.h"
+#include "blob.h"
+
+int main( int argc, char** argv )
+{
+    if(argc < 2)
+    {
+        printf("Veuillez specifier l'image binaire...\n");
+        return;
+    }
+
+    char* filename = argv[1];
+
+    IplImage* frame = cvLoadImage(filename, 0);
+    if(frame == NULL)
+    {
+        fprintf(stderr, "Erreur de lecture de l'image %s\n", filename);
+    }
+
+    Blob* blobs;
+    int blobCount = extractBlobs(frame, NULL, &blobs);
+    
+    printf("%d blobs trouves\n", blobCount);
+
+    int b;
+    for(b = 0; b < blobCount; b++)
+    {
+        printf("Label = %d\n", blobs[b].label);
+        printf("x = %d\n", blobs[b].box.x);
+    }
+
+    // Dessine les boites englobantes
+    drawBoundingRects(frame, blobs, blobCount);
+}
